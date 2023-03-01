0d7e928fa6b0102b4400c6a01f304227c222fffa
nrenars <114558077+nrenars@users.noreply.github.com>
2023-03-01 09:16:30 +0200

    Add files via upload

---
+++ Functions/vmartin.py
@@ -0,0 +1,19 @@
+def printShapes():
+    print('=======\n -x+x- \n')
+    print(' -x+x- ')
+    print('=======')
+
+
+
+name = input('What is your name?: ')
+printShapes()
+
+age = input('How old are you?: ')
+printShapes()
+
+
+drink = input('What is yout favorite drink?: ')
+printShapes()
+
+
+print(f'Hello {name}! You are {age} y.o.\nYour favorite drink is {drink}.')
